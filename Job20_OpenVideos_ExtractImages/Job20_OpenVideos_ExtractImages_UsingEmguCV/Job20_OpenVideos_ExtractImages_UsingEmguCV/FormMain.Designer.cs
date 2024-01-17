0dfaaaa599730a76285e5d882cb26690b9bf4a57
DungSatoru <Dungsatoru.dev@gmail.com>
2024-01-17 08:28:59 +0700

    Job19

---
+++ Job20_OpenVideos_ExtractImages/Job20_OpenVideos_ExtractImages_UsingEmguCV/Job20_OpenVideos_ExtractImages_UsingEmguCV/FormMain.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Job20_OpenVideos_ExtractImages_UsingEmguCV
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.btnOpenVideo = new System.Windows.Forms.Button();
+            this.btnExtractImage = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.WinMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.WinMediaPlayer)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Controls.Add(this.numericUpDown1);
+            this.panel2.Controls.Add(this.picBox);
+            this.panel2.Location = new System.Drawing.Point(498, 44);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(460, 560);
+            this.panel2.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(339, 19);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(121, 22);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(3, 53);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(457, 507);
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            // 
+            // btnOpenVideo
+            // 
+            this.btnOpenVideo.Location = new System.Drawing.Point(3, 3);
+            this.btnOpenVideo.Name = "btnOpenVideo";
+            this.btnOpenVideo.Size = new System.Drawing.Size(127, 38);
+            this.btnOpenVideo.TabIndex = 0;
+            this.btnOpenVideo.Text = "Open Video";
+            this.btnOpenVideo.UseVisualStyleBackColor = true;
+            this.btnOpenVideo.Click += new System.EventHandler(this.btnOpenVideo_Click);
+            // 
+            // btnExtractImage
+            // 
+            this.btnExtractImage.Location = new System.Drawing.Point(136, 3);
+            this.btnExtractImage.Name = "btnExtractImage";
+            this.btnExtractImage.Size = new System.Drawing.Size(127, 38);
+            this.btnExtractImage.TabIndex = 1;
+            this.btnExtractImage.Text = "Extract Images";
+            this.btnExtractImage.UseVisualStyleBackColor = true;
+            this.btnExtractImage.Click += new System.EventHandler(this.btnExtractImage_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btnExtractImage);
+            this.panel1.Controls.Add(this.btnOpenVideo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(958, 44);
+            this.panel1.TabIndex = 0;
+            // 
+            // WinMediaPlayer
+            // 
+            this.WinMediaPlayer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.WinMediaPlayer.Enabled = true;
+            this.WinMediaPlayer.Location = new System.Drawing.Point(3, 3);
+            this.WinMediaPlayer.Name = "WinMediaPlayer";
+            this.WinMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WinMediaPlayer.OcxState")));
+            this.WinMediaPlayer.Size = new System.Drawing.Size(461, 369);
+            this.WinMediaPlayer.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.BackColor = System.Drawing.SystemColors.Info;
+            this.panel3.Controls.Add(this.WinMediaPlayer);
+            this.panel3.Location = new System.Drawing.Point(0, 44);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(492, 560);
+            this.panel3.TabIndex = 2;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 604);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormMain";
+            this.Text = "Extract Images From Videos";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.WinMediaPlayer)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Button btnOpenVideo;
+        private System.Windows.Forms.Button btnExtractImage;
+        private System.Windows.Forms.Panel panel1;
+        private AxWMPLib.AxWindowsMediaPlayer WinMediaPlayer;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ Job20_OpenVideos_ExtractImages/Job20_OpenVideos_ExtractImages_UsingEmguCV/Job20_OpenVideos_ExtractImages_UsingEmguCV/FormMain.cs
@@ -0,0 +1,92 @@
+﻿using AxWMPLib;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Job20_OpenVideos_ExtractImages_UsingEmguCV
+{
+    public partial class FormMain : Form
+    {
+        private VideoCapture capture;
+        private int frameIndex = 0;
+        private bool capturing = false;
+        private Mat frame = new Mat(); // Khai báo ở đây để tránh tạo đối tượng mới mỗi lần Capture_ImageGrabbed được gọi
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenVideo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Chọn video";
+            ofd.Filter = "Tất cả các file|*.*|Video Files|*.avi;*.mp4;*.wmv|Tệp AVI|*.avi|Tệp MP4|*.mp4|Tệp WMV|*.wmv";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = ofd.FileName;
+                WinMediaPlayer.URL = selectedFilePath;
+                WinMediaPlayer.Ctlcontrols.play();
+
+                // Khởi tạo VideoCapture từ đường dẫn video
+                capture = new VideoCapture(selectedFilePath);
+            }
+        }
+
+        private async void btnExtractImage_Click(object sender, EventArgs e)
+        {
+            if (capture != null && !capturing)
+            {
+                capturing = true;
+
+                // Tạo một Task chạy quá trình trích xuất ảnh từ video
+                await Task.Run(() => ExtractImages());
+
+                MessageBox.Show("Hình ảnh đã được trích xuất và hiển thị trên PictureBox.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Đặt capturing về false sau khi quá trình trích xuất hoàn tất
+                capturing = false;
+            }
+            else
+            {
+                MessageBox.Show("Quá trình trích xuất đang diễn ra hoặc bạn chưa chọn một tệp video.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void ExtractImages()
+        {
+            while (capture != null && capture.Ptr != IntPtr.Zero && capture.Read(frame))
+            {
+                // Tính toán chiều rộng và chiều cao mới của frame dựa trên tỷ lệ khung hình
+                double aspectRatio = (double)frame.Width / frame.Height;
+                int newWidth = picBox.Width;
+                int newHeight = (int)(newWidth / aspectRatio);
+
+                // Resize frame theo chiều rộng và chiều cao của picBox
+                CvInvoke.Resize(frame, frame, new Size(newWidth, newHeight));
+
+                Image<Bgr, byte> img = frame.ToImage<Bgr, byte>();
+                Bitmap bitmap = img.ToBitmap();
+
+                // Hiển thị frame trên PictureBox
+                picBox.Invoke((MethodInvoker)delegate
+                {
+                    picBox.Image = bitmap;
+                    picBox.Invalidate();
+                });
+
+                // Tăng chỉ số frame
+                frameIndex++;
+            }
+
+            // Đã đọc hết video, dừng quá trình trích xuất
+            capture.Stop();
+            capture.Dispose();
+        }
+    }
+}
